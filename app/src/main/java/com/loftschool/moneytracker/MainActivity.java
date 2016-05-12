d2b801fc250cf5d9adc9f8b9e367eaccfc7a04ba
Yuri Tarasov <yutars@gmail.com>
2016-05-12 05:21:04 +0500

    create drawer

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -1,55 +1,67 @@
 package com.loftschool.moneytracker;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private  ActionBarDrawerToggle toggle;
+    private DrawerLayout drawerLayout; // -> findViewById missed!
+    private NavigationView navigationView; //-> findViewById missed!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupActionBar();
+        setupDrawerLayout();
+    }
 
-        Log.d(LOG_TAG,"onCreate()");
+
+    private void setupActionBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    private void setupDrawerLayout() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        setTitle(getString(R.string.app_name));
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG,"onStart()");
+    public boolean onNavigationItemSelected(MenuItem item) {
+        if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        return true;
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG,"onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG,"onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG,"onStop()");
-
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG,"onDestroy()");
-
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG,"onRestart()");
-
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
